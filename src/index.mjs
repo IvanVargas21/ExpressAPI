4eeb78673a3800a061e20ef6a650ac97a24d8706
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-07-17 16:57:14 +0800

    Added: cookie-parser middlware and added signed cookie on root url path

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,16 @@
 import express from 'express';
 import { loggingMiddleware } from './middleware/middlewares.mjs';
 import routes from './routes/index.mjs';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
 // parses incoming JSON reqs and puts the parsed data in req.body
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser('my_secret_key'));
+
 app.use(loggingMiddleware, (req, res, next) =>{
     console.log('Finished Logging...');
     next(); // continues to route handlers
@@ -17,9 +20,9 @@ app.use(loggingMiddleware, (req, res, next) =>{
 app.use(routes);
 
 app.get('/', (req, res) => {
+    res.cookie('name','Ivan Vargas', { maxAge: 60000 * 60, signed: true});
     res.status(201).send({
         message: 'Welcome Ivan Vargas',
-        status: 'success'
     });
 })
 
