4fcfa75864dba5c9effd64e4581fc8d72b759e39
ivanvargass <165545622+IvanVargas21@users.noreply.github.com>
2025-07-17 16:57:46 +0800

    Added: cookie checker on product route

--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -4,7 +4,15 @@ const router = Router();
 
 router.get(
     '/api/products', (req, res) =>{
-    res.send(mockProducts)
+    console.log(req.headers.cookie);
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    console.log(req.signedCookies.name)
+    if(req.signedCookies.name === 'Ivan Vargas')
+    return res.send(mockProducts)
+    
+    
+    res.status(403).send({msg: 'Sorry, You need the correct cookie to access this resource'})
 }
 )
 
